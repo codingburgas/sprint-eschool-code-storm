239fab665ae9d1cce206d4a2988d8411e7a87bec
NTSManolov23 <NTSManolov23@codingburgas.bg>
2025-03-25 01:23:21 +0200

    Add main cpp and sep.

---
+++ EquationSolvation/main.cpp
@@ -0,0 +1,42 @@
+#include "quiz.h"
+#include "subjects.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    srand(time(0));
+    InitWindow();
+
+    int selectedOption = 0;
+    bool isRunning = true;
+
+    while (!WindowShouldClose() && isRunning) {
+        if (IsKeyPressed(KEY_DOWN)) {
+            selectedOption = (selectedOption + 1) % 6;
+        }
+        if (IsKeyPressed(KEY_UP)) {
+            selectedOption = (selectedOption - 1 + 6) % 6;
+        }
+        if (IsKeyPressed(KEY_ENTER)) {
+            switch (selectedOption) {
+            case 0: mathematics(); break;
+            case 1: bulgarianLanguage(); break;
+            case 2: history(); break;
+            case 3: geography(); break;
+            case 4: biology(); break;
+            case 5: isRunning = false; break;
+            }
+        }
+
+        BeginDrawing();
+        ClearBackground(backgroundColor);
+        DrawMenu(selectedOption);
+        DrawScore();
+        EndDrawing();
+    }
+
+    CloseWindow();
+    std::cout << "Thank you for using the Digital School! Your final score is: " << score << " points.\n";
+    return 0;
+}
---
+++ EquationSolvation/subjects.h
@@ -0,0 +1,13 @@
+#ifndef SUBJECTS_H
+#define SUBJECTS_H
+
+#include <vector>
+#include "quiz.h"
+
+void mathematics();
+void bulgarianLanguage();
+void history();
+void geography();
+void biology();
+
+ 
